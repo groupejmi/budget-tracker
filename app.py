906063c5158c4b32dbb1918154ca8405ddb4fd12
groupejmi <m.djiguine@groupejmi.fr>
2025-11-04 12:48:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(page_title=" Tableau de bord Budget", layout="wide")
 @st.cache_data
 def load_data():
     file_path = r'budget_v2.csv'
-    data_set = pd.read_csv(file_path, sep=',', encoding='utf-8')
+    data_set = pd.read_csv(file_path, sep=';', encoding='utf-8')
     data = data_cleaning(data_set)
     return data
 
@@ -104,6 +104,7 @@ with tab1:
 with tab2:
     st.header("🧮 Analyse détaillée")
     st.dataframe(df_filtered)
+
 
 
 
